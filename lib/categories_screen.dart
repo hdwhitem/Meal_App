f682a41f2a9d263e9b2c4a801751b6e2ce4351ee
hdwhitem <hdwhite.m@gmail.com>
2022-04-29 13:42:21 +0100

    Styling & Theming

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -9,9 +9,10 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('DeliMeals'),
+        title: const Text('DeliMeal'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map(
               (catDate) => CategoryItem(
--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
+      title: 'DeliMeal',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Colors.pink,
+          secondary: Colors.amber, //accent color
+        ),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              // titleLarge: const TextStyle(
+              //   fontSize: 24,
+              //   fontFamily: 'RobotoCondensed',
+              // ),
+            ),
       ),
       home: CategoriesScreen(),
     );
